4b4596611bd8df039ce353ac99d9d4c7c9c75b65
Lucas Negri <91978524+lucasnegri@users.noreply.github.com>
2023-01-30 16:38:19 -0300

    First finished layout
    This contains the base of the website. Responsible and minimal.

---
+++ project4/network/static/js/main.js
@@ -0,0 +1,11 @@
+
+const buttons = document.querySelectorAll(".button_header");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        buttons.forEach(function(b) {
+            b.classList.remove("active");
+        });
+        this.classList.add("active");
+    });
+});
