5bf0ba98e57eeca745ea78016d1bac998c66b760
Lucas Negri <91978524+lucasnegri@users.noreply.github.com>
2023-02-08 21:55:09 -0300

    Add "Manage Team" section
    The section contains all the information about the team, with the possibility to remove or add members to it.

--- project4/network/static/js/create_project.js
+++ project4/network/static/js/create_project.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function() {
         usersCheckbox.innerHTML = "";
         data.users.forEach(user => {
         usersCheckbox.innerHTML += `
-        <div class="form-check mb-1"> 
+        <div class="form-check mb-1">
             <input class="form-check-input" id="flexCheck1" name="user[]" value="${user.username}" type="checkbox"> 
             <label class="form-check-label" for="flexCheck1"><span class="fst-italic pl-1">${user.username}</span></label> 
         </div> `;
@@ -20,19 +20,19 @@ window.addEventListener("load", function() {
 
 // Function to update user_list being passed when the user select a "team" on the selection input
 document.getElementById("selected_team").addEventListener("change", function() {
-  const teamId = this.value;
-  fetch(`/get_team_users/${teamId}`)
-      .then(response => response.json())
-      .then(data => {
-          const usersCheckbox = document.getElementById("user_checkbox");
-          usersCheckbox.innerHTML = "";
-          data.users.forEach(user => {
-              usersCheckbox.innerHTML += `
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="checkbox1" name="user[]" value="${ user.username }">
-                    <label class="form-check-label" for="checkbox1">${user.username }</label>
-                </div>
-              `;
-          });
-      });
+    const teamId = this.value;
+    fetch(`/get_team_users/${teamId}`)
+        .then(response => response.json())
+        .then(data => {
+            const usersCheckbox = document.getElementById("user_checkbox");
+            usersCheckbox.innerHTML = "";
+            data.users.forEach(user => {
+                usersCheckbox.innerHTML += `
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="checkbox1" name="user[]" value="${ user.username }">
+                        <label class="form-check-label" for="checkbox1">${user.username }</label>
+                    </div>
+                `;
+            });
+        });
 });
